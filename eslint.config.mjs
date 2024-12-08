918cc782a1c6732039224dc3348fd18734b23a02
Jonathan Perry <jonathanperry@sequencemedia.net>
2024-12-08 15:13:08 +0000

    `@sequencemedia/eslint-config-standard` `@sequencemedia/eslint-config-typescript`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import globals from 'globals'
-import standard from '@sequencemedia/eslint-config-standard/merge/recommended'
-import typescript from '@sequencemedia/eslint-config-typescript/merge/recommended'
+import {
+  configs as standard
+} from '@sequencemedia/eslint-config-standard'
+import {
+  configs as typescript
+} from '@sequencemedia/eslint-config-typescript'
 import babelParser from '@babel/eslint-parser'
 import typescriptParser from '@typescript-eslint/parser'
 import reactPlugin from 'eslint-plugin-react'
@@ -56,7 +60,8 @@ export default [
       '**/*.{jsx,tsx}'
     ]
   },
-  standard({
+  {
+    ...standard.recommended,
     files: [
       '**/*.{mjs,cjs}'
     ],
@@ -65,48 +70,61 @@ export default [
       'stories'
     ],
     languageOptions: {
+      ...standard.recommended.languageOptions,
       globals: {
+        ...standard.recommended.globals,
         ...globals.node
       }
     }
-  }),
-  standard({
+  },
+  {
+    ...standard.recommended,
     files: [
       'src/**/*.{mjs,cjs}'
     ],
     languageOptions: {
+      ...standard.recommended.languageOptions,
       globals: {
+        ...standard.recommended.languageOptions.globals,
         ...globals.browser
       }
     }
-  }),
+  },
   /**
    *  Standard config for all `jsx` and `tsx` files
    */
-  standard({
+  {
+    ...standard.recommended,
     files: [
       '**/*.{jsx,tsx}'
     ],
     languageOptions: {
+      ...standard.recommended.languageOptions,
       parser: babelParser,
       parserOptions: {
+        ...standard.recommended.languageOptions.parserOptions,
         ...reactParserOptions
       },
       globals: {
+        ...standard.recommended.globals,
         ...globals.browser
       }
     },
     plugins: {
+      ...standard.recommended.plugins,
       ...reactPlugins
     },
     rules: {
+      ...standard.recommended.rules,
       ...reactRules
     },
     settings: {
+      ...standard.recommended.settings,
       ...reactSettings
     }
-  }),
-  typescript({
+  },
+  {
+    ...typescript.recommended,
     files: [
       '**/*.{mts,cts}'
     ],
@@ -115,46 +133,57 @@ export default [
       'stories'
     ],
     languageOptions: {
+      ...typescript.recommended.languageOptions,
       globals: {
+        ...typescript.recommended.languageOptions.globals,
         ...globals.node
       }
     }
-  }),
-  typescript({
+  },
+  {
+    ...typescript.recommended,
     files: [
       'src/**/*.{mts,cts}'
     ],
     languageOptions: {
+      ...typescript.recommended.languageOptions,
       globals: {
+        ...typescript.recommended.languageOptions.globals,
         ...globals.browser
       }
     }
-  }),
+  },
   /**
    *  TypeScript config for only `tsx` files
    */
-  typescript({
+  {
+    ...typescript.recommended,
     files: [
       'src/**/*.tsx'
     ],
     languageOptions: {
+      ...typescript.recommended.languageOptions,
       parser: typescriptParser,
       parserOptions: {
-        ...reactParserOptions,
-        project: 'tsconfig.json'
+        ...typescript.recommended.languageOptions.parserOptions,
+        ...reactParserOptions
       },
       globals: {
+        ...typescript.recommended.languageOptions.globals,
         ...globals.browser
       }
     },
     plugins: {
+      ...typescript.recommended.plugins,
       ...reactPlugins
     },
     rules: {
+      ...typescript.recommended.rules,
       ...reactRules
     },
     settings: {
+      ...typescript.recommended.settings,
       ...reactSettings
     }
-  })
+  }
 ]
